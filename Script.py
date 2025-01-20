bc1077defdc8695568c8f9aaafc0c15bf12c1ce5
sandeep808481 <rathodsandeep43071@gmail.com>
2025-01-20 20:03:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -253,14 +253,14 @@ Nᴀᴍᴇ - {}"""
 
 IMDb Data:
 
-<b>🏷 Title</b>: <a href={url}>{title}</a>
+<b>🧿 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
+🎙️ Languages : <code>{languages}</code>
 📀 RunTime: {runtime} Minutes
 📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
+★ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : @Movie_Hobbies
 
 
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
