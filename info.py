a578777747d9238d7ca076f86ae037924609a411
appuboss12 <appuforever638@gmail.com>
2025-04-23 17:09:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,9 +126,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'setURL.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '18ed4a3810dda2c8a07ae586d0e4b73c256508ed')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_link_open_z/5') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'LinkCents.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '2273288333134c87496059f5414c34317ecec8d8')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Link_SK') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
